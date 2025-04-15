90c02d73b2c293c6a2334567cc537acc06d21405
unknown <fikaboteekay@gmail.com>
2025-04-15 15:22:17 +0100

    Chapter 5

--- RandomClassGenerator.java
+++ RandomClassGenerator.java
@@ -12,23 +12,5 @@ public class RandomClassGenerator{
 		
 		int integerWithBound = random.nextInt(1050)  + 1;
 		System.out.printf("The random number is %d%n",integerWithBound);
-		
-		long integerWithNoBound = random.nextLong();
-		System.out.printf("The random number is %d%n",integerWithNoBound);
-		
-		long integerWithBound = random.nextLong(1050)  + 1;
-		System.out.printf("The random number is %d%n",integerWithBound);
-		
-		float integerWithNoBound = random.nextFloat();
-		System.out.printf("The random number is %d%n",integerWithNoBound);
-		
-		float integerWithBound = random.nextFloat(1050)  + 1;
-		System.out.printf("The random number is %d%n",integerWithBound);
-		
-		double integerWithNoBound = random.nextInt();
-		System.out.printf("The random number is %d%n",integerWithNoBound);
-		
-		double integerWithBound = random.nextInt(1050)  + 1;
-		System.out.printf("The random number is %d%n",integerWithBound);
 	}
 }	
